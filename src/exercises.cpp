4561b921882ef82dd5265291604ec47360611555
Mijael-Callejas <159574812+Mijael-Callejas@users.noreply.github.com>
2024-04-02 18:53:10 -0400

    dgvs

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -39,7 +39,12 @@ void exercise_5(int n, int k) {
 }
 
 int exercise_6(int n) {
-  // TODO: YOUR CODE HERE
+    int sum = 0;
+    while(n > 0){
+        sum = sum + n%10;
+        n = n/10;
+    }
+    cout << sum << endl;
   return 0;
 }
 
