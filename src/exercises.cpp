ba842e23a13a71e869762b21946bdec04e4ae637
Mijael-Callejas <159574812+Mijael-Callejas@users.noreply.github.com>
2024-04-02 16:53:49 -0400

    asdasdafs

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -18,15 +18,9 @@ void exercise_2(string s1) {
 }
 
 void exercise_3(string s1) {
-int nom = 0;
-int no_cake = 0;
 getline(cin, s1, '\n');
 for(int i = 0; i < 10; i++){
-switch(s1[i]) {
-    case 0: no_cake++; break;
-    case 1: nom++; break;
-}
-if(no_cake >= 1){
+if(s1[i] == 0){
 cout << "No cake :(" << endl;
 break;
 }
