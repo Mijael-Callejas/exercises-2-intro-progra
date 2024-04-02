1506bbac11fe872e19fe7539190146f627915890
Mijael-Callejas <159574812+Mijael-Callejas@users.noreply.github.com>
2024-04-02 18:56:19 -0400

    asdczsasdsc

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -39,13 +39,13 @@ void exercise_5(int n, int k) {
 }
 
 int exercise_6(int n) {
+cin >> n;
     int sum = 0;
     while(n > 0){
         sum = sum + n%10;
         n = n/10;
     }
     cout << sum << endl;
-  return 0;
 }
 
 void exercise_7(int n) {
