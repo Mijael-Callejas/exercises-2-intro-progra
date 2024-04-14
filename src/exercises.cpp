4cd608a1b9b27a4f4a831e292bb47fd4ad270932
Mijael-Callejas <159574812+Mijael-Callejas@users.noreply.github.com>
2024-04-14 00:11:48 -0400

    asfasdasd

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -115,13 +115,17 @@ void exercise_5(int n, int k) {
     cout << " " << days << " ";
     }
     else if (days < 10 && dsem == 7){
-    cout << " " << days << endl;
+    cout << " " << days;
+    }
+    if(days > 10 && dsem < 7){
+    cout << days << " ";
     }
     else if(days > 10 && dsem < 7){
     cout << days << " ";
     }
-    else if(days > 10 && dsem == 7){
-    cout << days << endl;
+    if (dsem == 7){
+        cout << endl;
+        dsem = 0;
     }
     }
     cout << endl;
